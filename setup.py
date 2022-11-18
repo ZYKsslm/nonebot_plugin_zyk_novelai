3bf5c04487e1b91a0ae118544f4d269126e6887c
ZYKsslm <3119964735@qq.com>
2022-11-18 23:23:41 +0800

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="nonebot_plugin_zyk_novelai",
+    version="2022.11.18.1",
+    packages=find_packages(),
+    author="ZSSLM",
+    author_email="3119964735@qq.com",
+    description="This is a simple plugin that is to send your image made by your NovelAI for nonebot2",
+    url="https://github.com/ZYKsslm/nonebot_plugin_zyk_novelai",
+    license="Apache License 2.0",
+    requires=["nonebot", "fake_useragent", "httpx", "nonebot2", "nonebot_adapter_onebot"],
+)

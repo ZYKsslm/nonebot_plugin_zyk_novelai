2f5af2e5c7f2bf9d5e749724404c708c4b2a8327
ZYKsslm <3119964735@qq.com>
2022-11-25 22:34:13 +0800

    :pushpin: version 2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonebot_plugin_zyk_novelai",
-    version="2.1.1",
+    version="2.2",
     packages=find_packages(),
     author="ZSSLM",
     author_email="3119964735@qq.com",
@@ -11,6 +11,6 @@ setup(
     description="have updated the database",
     url="https://github.com/ZYKsslm/nonebot_plugin_zyk_novelai",
     license="MIT License",
-    requires=["fake_useragent", "httpx", "nonebot2", "nonebot_adapter_onebot"],
+    requires=["fake_useragent", "httpx", "colorama", "nonebot2", "nonebot_adapter_onebot"],
     package_data={"nonebot_plugin_zyk_novelai": ["resource/*"]}
 )

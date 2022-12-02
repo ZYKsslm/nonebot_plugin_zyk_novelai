af9771ef6dc63dd3bc141b75f1bba9005c49eb72
ZYKsslm <3119964735@qq.com>
2022-12-02 22:52:58 +0800

    :pushpin: version 2.7.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nonebot_plugin_zyk_novelai",
-    version="2.7",
+    version="2.7.1",
     packages=find_packages(),
     author="ZSSLM",
     author_email="3119964735@qq.com",
